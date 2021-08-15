01c1b64db9a51840c5396aa647d007847657f64b
JhonyDev <star.jk901@gmail.com>
2021-08-16 02:36:50 +0500

    before deployment

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework import routers
-from django.conf.urls import url, include
 
 from .views import api_post_photo, api_post_user, api_post_interest, api_post_exp, api_post_report, \
     api_delete_interest, api_delete_photo, api_show_image, api_get_exp, MpesaTransactionsViewSet, MpesaSTKApiView, \
@@ -24,13 +23,11 @@ urlpatterns = [
     path('get_user_images', get_user_images),
     path('get_user_interests', get_interest),
 
-    url(r'^get_user/$', get_user),
-
-    # path('get_user', get_user),
+    path('get_user', get_user),
 
     path('get_exp', api_get_exp),
 
-    path('run_cron', run_cron(), name='cron'),
+    path('run_cron', run_cron, name='cron'),
 
     path('mpesa-stk-push/', MpesaSTKApiView.as_view(),
          name='mpesa_stk_push'),
