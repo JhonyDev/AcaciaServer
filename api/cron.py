6f17694b77b1a338aa1fb787f0c06d4c3eb3e08b
JhonyDev <star.jk901@gmail.com>
2021-08-16 17:12:38 +0500

    before deployment

--- api/cron.py
+++ api/cron.py
@@ -3,6 +3,29 @@ from django.http import HttpResponse
 from .models import User
 from admin_panel.models import PaidUsers, UnPaidUsers
 
+from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import auth
+
+cred = credentials.Certificate("/home/adminuser/AcaciaServer/mate-1d51c-firebase-adminsdk-r3icd-f455e2461f.json")
+firebase_admin.initialize_app(cred)
+
+
+def init_firebase(request):
+    page = auth.list_users()
+    a_list = None
+    for user in page.users:
+        a_list.append(str(user.uid))
+
+    result = auth.delete_users(a_list)
+
+    print('Successfully deleted {0} users'.format(result.success_count))
+    print('Failed to delete {0} users'.format(result.failure_count))
+    for _ in result.errors:
+        print('error #{0}, reason: {1}'.format(result.index, result.reason))
+
+    return HttpResponse("finished")
+
 
 def run_cron():
     print('Cron running')
@@ -51,7 +74,12 @@ def run_cron():
         if not new_list_paid_user:
             continue
 
-        print("Adding to paid users")
+        if paid_user.paid_fee:
+            print('Already paid enabled')
+            continue
+
+        print()
+        print("Adding to paid users = ", paid_user.name)
 
         paid_user.paid_fee = True
         paid_user.save()
